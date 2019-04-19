3dd435550285f61d5849b051d4fad22cbbca2625
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2019-04-19 18:14:12 +0200

    Add keyboard option to firmwares script.

--- change-firmware.sh
+++ change-firmware.sh
@@ -4,9 +4,16 @@ argument="$1"
 firmwares="$(dirname "$(readlink -f "$0")")/USBKeyboard/firmware"
 dfuprogrammercmd="sudo dfu-programmer atmega16u2"
 
+function chfirmware() {
+    $dfuprogrammercmd erase
+    $dfuprogrammercmd ${firmwares}/$1.hex
+    $dfuprogrammercmd reset
+}
+
 if [[ -z $argument ]] ; then
   echo "Available options:"
-  echo "d[efault] - the standard firmware, allows uploading snippets with e.g. IDE"
+  echo "d[efault]  - the standard firmware, allows uploading snippets with e.g. IDE"
+  echo "k[eyboard] - keyboard-HID firmware. For actually using the device."
   exit 1
 else
   echo "This scripts assumes you have an Arduino Uno connected and the USB controller reset."
@@ -14,10 +21,12 @@ else
   set -e
   case $argument in
    d|default)
-    $dfuprogrammercmd erase
-    $dfuprogrammercmd ${firmwares}/Arduino-usbserial-uno.hex
-    $dfuprogrammercmd reset
+    chfirmware "Arduino-usbserial-uno"
     echo "Changed firmware to Arduino-usbserial."
+    ;;
+   k|keyboard)
+    chfirmware "Arduino-keyboard-0.3"
+    echo "Changed firmware to Arduino-keyboard."
     ;;
   esac
 fi
